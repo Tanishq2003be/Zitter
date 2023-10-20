25fec073b2a6f4766f09fc9f7160e68377f93821
Tanishq Dublish <tdublish_be21@thapar.edu>
2023-10-21 01:20:39 +0530

    added

--- pages/api/current.tsx
+++ pages/api/current.tsx
@@ -5,7 +5,7 @@ import serverAuth from '@/libs/serverAuth';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') {
     return res.status(405).end();
-  } 
+  }
 
   try {
     const { currentUser } = await serverAuth(req, res);
